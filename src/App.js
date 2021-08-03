6d56162732b968c40be2683bbb83a0d13afc2312
shubhamcoder30 <shubhams.48singh@gmail.com>
2021-08-03 07:43:01 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import {Table,Button} from "react-bootstrap";
-
+import { Table, Button } from 'react-bootstrap';
 
 function App() {
   const [page, setPage] = useState(1);
@@ -15,7 +14,6 @@ function App() {
     fetch(`https://reqres.in/api/users?page=${page}`).then(result => {
       result.json().then(resp => {
         setData(resp.data);
-      
       });
     });
   }, [page]);
@@ -26,30 +24,31 @@ function App() {
         <h1> FETCH API {page}</h1>
 
         <Table className="table" striped bordered hover variant="dark">
-        
-        <thead>
-          <tr>
-            <th>id no</th>
-            <th>Firstname</th>
-            <th>Lastname</th>
-            <th>email</th>
-          </tr>
+          <thead>
+            <tr>
+              <th>id no</th>
+              <th>Firstname</th>
+              <th>Lastname</th>
+              <th>email</th>
+            </tr>
           </thead>
           <tbody>
-          {
-      
-          data.map((item, i) => (
-            <tr>
-              <td>{i}</td>
-              <td>{item.first_name}</td>
-              <td>{item.last_name}</td>
-              <td>{item.email}</td>
-            </tr>
-          ))}
+            {data.map((item, i) => (
+              <tr>
+                <td>{item.id}</td>
+                <td>{item.first_name}</td>
+                <td>{item.last_name}</td>
+                <td>{item.email}</td>
+              </tr>
+            ))}
           </tbody>
         </Table>
-        <Button variant="dark" onClick={firstPage}>page 1</Button>
-        <Button variant="dark" onClick={secondPage}>page 2</Button>
+        <Button variant="dark" onClick={firstPage}>
+          page 1
+        </Button>
+        <Button variant="dark" onClick={secondPage}>
+          page 2
+        </Button>
       </>
     );
   }
